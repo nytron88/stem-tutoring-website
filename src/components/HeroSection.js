c17c34a03b8847b84d6b4a1ee61eb4a0d8be5d4e
Siddhant Jain <sidjain88tx@gmail.com>
2023-12-04 14:07:24 -0600

    added buttons hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,6 +7,12 @@ function HeroSection() {
       <h1>STEM Tutoring Club</h1>
       <button>About Us</button>
       <a href="/appointments"><button>Appointments</button></a>
+      <div className="subject-buttons">
+        <a href="math-resources"><button className="subject-button ib-maths">Math</button></a>
+        <a href="biology-resources"><button className="subject-button ib-biology">Biology</button></a>
+        <a href="chemistry-resources"><button className="subject-button ib-chemistry">Chemistry</button></a>
+        <a href="/physics-resources"><button className="subject-button ib-physics">Physics</button></a>
+      </div>
     </div>
   );
 }
